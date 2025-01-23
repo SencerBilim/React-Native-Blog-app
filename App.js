219e604a68533532cbd1f20b919ee6bbeee5097a
SencerBilim <sencer.bilim@gmail.com>
2025-01-23 16:15:28 +0300

    first commit

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import IndexScreen from './src/screens/IndexScreens';
+
 
 export default function App() {
   return (
