56450488aee438b9ca8ef0dfaeff47afc67606d2
SencerBilim <sencer.bilim@gmail.com>
2025-01-24 17:32:27 +0300

    state and context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
+    const [ blogPosts, setBlogPosts] = useState([]);
 
-    return <BlogContext.Provider value={10}>
+    const addBlogPost = () => {
+        setBlogPosts([...blogPosts, { title:`Blog Post #${blogPosts.length +1}` }]);
+    }
+
+    return <BlogContext.Provider value={{data: blogPosts, addBlogPost }}>
         {children}
     </BlogContext.Provider>
 }
