4d3aeef0fd1e33ea0c603d5433add54be75199f5
SencerBilim <sencer.bilim@gmail.com>
2025-01-24 17:05:56 +0300

    added context

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import IndexScreen from './src/screens/IndexScreens';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import { BlogProvider } from './src/context/BlogContext';
 
+
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <BlogProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Blog">
+          <Stack.Screen name="Blog" component={IndexScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </BlogProvider>
   );
 }
 
