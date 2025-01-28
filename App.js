ef079205c35db52326dade8f4e76e389d4be65a1
SencerBilim <sencer.bilim@gmail.com>
2025-01-28 16:36:11 +0300

    styling

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import IndexScreen from './src/screens/IndexScreens';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
-import { BlogProvider } from './src/context/BlogContext';
+import { Provider } from './src/context/BlogContext';
 
 
 
@@ -12,13 +12,13 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <BlogProvider>
+    <Provider>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Blog">
           <Stack.Screen name="Blog" component={IndexScreen} />
         </Stack.Navigator>
       </NavigationContainer>
-    </BlogProvider>
+    </Provider>
   );
 }
 
--- src/screens/IndexScreens.js
+++ src/screens/IndexScreens.js
@@ -1,26 +1,46 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet, FlatList, Button } from "react-native";
-import BlogContext from "../context/BlogContext";
-
+import { Context } from "../context/BlogContext";
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 
 
 const IndexScreen = () => {
-    const { data, addBlogPost } = useContext(BlogContext)
+    const { state, addBlogPost } = useContext(Context)
 
     return <View>
         <Button title="Add Post" onPress={addBlogPost}/>
         <FlatList 
-        data={data}
+        data={state}
         keyExtractor={(blogPosts) => blogPosts.title}
         renderItem={({item}) => {
-            return <Text> {item.title}</Text>
+            return <View style={styles.row}> 
+                <Text style={styles.title}> {item.title}</Text>
+                <MaterialCommunityIcons name="delete" style={styles.icon} />
+            </View>
         }}
         />
     
     </View>
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingVertical: 15,
+        paddingHorizontal: 15,
+        borderTopWidth: 1,
+        borderColor: "gray",
+        
+    },
+    title: {
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 24,
+        color: "grey"
+    }
+});
 
 export default IndexScreen;
